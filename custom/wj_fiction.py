c2d8f13036c782b750519c46e1fae0941f815466
贺文杰 <wx2018543@dev.bcs>
2021-02-02 22:51:30 +0800

    爬取网络小说初步完成，vip部分还有待优化，以及模拟用户信息、游览器访问

--- custom/wj_fiction.py
+++ custom/wj_fiction.py
@@ -4,22 +4,25 @@ from urllib import robotparser
 import ssl
 import requests
 import re
+import bs4
+from bs4 import BeautifulSoup
+from lxml import etree
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 #识别网站所用技术的工具
-zhulang = builtwith.parse('http://book.zhulang.com')
-print(zhulang)
+# zhulang = builtwith.parse('http://book.zhulang.com')
+# print(zhulang)
 
 #查询网站所有者的工具
-owner = whois.whois('book.zhulang.com')
-print(owner)
+# owner = whois.whois('book.zhulang.com')
+# print(owner)
 
 #解析robots.txt
-parser = robotparser.RobotFileParser()
-parser.set_url('http://book.zhulang.com/robots.txt')
-parser.read()
-print(parser)
+# parser = robotparser.RobotFileParser()
+# parser.set_url('http://book.zhulang.com/robots.txt')
+# parser.read()
+# print(parser)
 
 def html_content(url):
     resp = requests.get(url)
@@ -27,11 +30,22 @@ def html_content(url):
         for charset in ('utf-8', ):
             try:
                 content = resp.content.decode(charset)
-                # print(content)
-                res = r'</style>.*?<p>(.*?)<p>.*?<cite>'
-                textList = re.findall(res, content)
-                print(textList)
-
+                soup = BeautifulSoup(content,'lxml')
+                for tag in soup.find_all('p', 'cmt-sub fr'):
+                    tag.decompose()
+                for tag in soup.find_all('div', 'cmt-list'):
+                    tag.decompose()
+                for tag in soup.find_all('div', 'hidden-cnt catalog vol-box'):
+                    tag.decompose()
+                for tag in soup.find_all('div', 'book-rcm-con'):
+                    tag.decompose()
+                allP = soup.find_all('p')
+                for on in allP:
+                    s = on.find('a')
+                    if s == None:
+                        print(on.get_text())
+                    else:
+                        pass
             except UnicodeDecodeError:
                 pass
 
@@ -50,7 +64,7 @@ def main():
                 titleList = [x.strip() for x in titleList]
                 print(linkList)
                 print(titleList)
-                url = linkList[0]
+                url = linkList[41]
                 html_content(url)
             except UnicodeDecodeError:
                 pass
